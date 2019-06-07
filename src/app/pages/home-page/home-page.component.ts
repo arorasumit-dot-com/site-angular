0ee8e9b63c0efa854f39c42448ada61da3848f77
Sumit Arora <er.sumitarora@gmail.com>
2019-06-07 16:37:44 -0400

    adding animations

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,40 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
   animations: [
-    trigger('heroState', [
+    trigger('titleState', [
       state(
         'inactive',
         style({
-          transform: 'scale(1)',
+          opacity: 0,
         }),
       ),
       state(
         'active',
         style({
-          transform: 'scale(1.2)',
+          opacity: 1,
         }),
       ),
-      transition('inactive => active', animate('100ms ease-in')),
-      transition('active => inactive', animate('100ms ease-out')),
+      transition('inactive => active', animate('200ms ease-in')),
+      transition('active => inactive', animate('500ms ease-out')),
+    ]),
+    trigger('subTitleState', [
+      state(
+        'inactive',
+        style({
+          transform: 'scale(0)',
+        }),
+      ),
+      state(
+        'active',
+        style({
+          transform: 'scale(1)',
+        }),
+      ),
+      transition('inactive => active', animate('500ms ease-in')),
+      transition('active => inactive', animate('1000ms ease-out')),
+    ]),
+    trigger('socialState', [
+      state(
+        'inactive',
+        style({
+          opacity: 0,
+          transform: 'scale(0)',
+        }),
+      ),
+      state(
+        'active',
+        style({
+          opacity: 1,
+          transform: 'scale(1) rotate(360deg)',
+        }),
+      ),
+      transition('inactive => active', animate('1000ms ease-in')),
+      transition('active => inactive', animate('2000ms ease-out')),
     ]),
   ],
 })
 export class HomePageComponent implements OnInit {
   state = 'inactive';
   ngOnInit() {
-    // setTimeout(() => {
-    //   this.state = 'active';
-    // }, 5 * 1000);
+    setTimeout(() => (this.state = 'active'), 500);
   }
 }
